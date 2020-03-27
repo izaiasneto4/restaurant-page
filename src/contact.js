7aa538b0cb4c85f4f704005446775ac845f427f1
Izaias Neto <izaiasneto4@gmail.com>
2020-03-27 15:10:24 -0300

    Changed the toggleContent function

--- src/contact.js
+++ src/contact.js
@@ -1,36 +1,32 @@
-const contact = `
-<main class="main-form">
-  <form>
-    <label for="name">Name: </label>
-    <input type="text" placeholder="Ex: John" name="name">
-    <label for="name">Last Name: </label>
-    <input type="text" placeholder="Ex: Smith" name="last-name">
-    <label for="name">Email: </label>
-    <input type="email" placeholder="Ex: john@smith.com" name="email">
-    <label for="message">Your message:</label>
-    <textarea name="message" id="" cols="30" rows="10"></textarea>
-    <input type="submit" >
-  </form>
-</main>
-`;
 
 const contact = () => {
   const mainForm = document.createElement('main').classList.add('main-form');
-  const form = document.createElement('form');
-  const nameLabel = document.createElement('label').setAttribute('for', 'name');
-  const nameInput = document.createElement('input').setAttribute('type', 'text').setAttribute('name', 'name');
-  const lNameLabel = document.createElement('label').setAttribute('for', 'last-name');
-  const lNameInput = document.createElement('input').setAttribute('type', 'text').setAttribute('name', 'last-name');
-  const emailLabel = document.createElement('label').setAttribute('for', 'email');
-  const emailInput = document.createElement('input').setAttribute('name', 'email');
-  const messageLabel = document.createElement('label').setAttribute('for', 'message');
-  const message = document.createElement('textarea').setAttribute('cols', 30).setAttribute('rows', 10).setAttribute('name', 'message');
-  const submit = document.createElement('input').setAttribute('type', 'submit');
-
-  mainForm.appendChild(form);
-
+  // const form = document.createElement('form');
+  // form.innerHTML = 'hey this is a form';
+  // mainForm.innerHTML(form);
   return mainForm;
-}
+  // form.innerHTML = 'this is an example form';
+  // const nameLabel = document.createElement('label').setAttribute('for', 'name');
+  // const nameInput = document.createElement('input').setAttribute('type', 'text').setAttribute('name', 'name');
+  // const lNameLabel = document.createElement('label').setAttribute('for', 'last-name');
+  // const lNameInput = document.createElement('input').setAttribute('type', 'text').setAttribute('name', 'last-name');
+  // const emailLabel = document.createElement('label').setAttribute('for', 'email');
+  // const emailInput = document.createElement('input').setAttribute('name', 'email');
+  // const messageLabel = document.createElement('label').setAttribute('for', 'message');
+  // const message = document.createElement('textarea').setAttribute('cols', 30).setAttribute('rows', 10).setAttribute('name', 'message');
+  // const submit = document.createElement('input').setAttribute('type', 'submit');
+
+  // form.appendChild(nameLabel);
+  // form.appendChild(nameInput);
+  // form.appendChild(lNameLabel);
+  // form.appendChild(lNameInput);
+  // form.appendChild(emailLabel);
+  // form.appendChild(emailInput);
+  // form.appendChild(messageLabel);
+  // form.appendChild(message);
+  // form.appendChild(submit);
+  // return mainForm;
+};
 
 
 export default contact;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import './styles.css';
 import menu from './menu';
 import contact from './contact';
+import footer from './footer';
 
 const menuBtn = document.querySelector('.menu-btn');
 const contactBtn = document.querySelector('.contact-btn');
-const mainSection = document.querySelector('.main');
+const mainSection = document.createElement('div').classList.add('main');
 
 const toggleContent = (content) => {
-  mainSection.innerHTML = content;
+  mainSection.innerHTML = '';
+  mainSection.appendChild(content);
+  mainSection.appendChild(footer());
+  document.body.appendChild(mainSection);
 };
 
 menuBtn.addEventListener('click', () => toggleContent(menu));
 
 contactBtn.addEventListener('click', () => {
-  toggleContent(contact);
+  toggleContent(contact());
 });
