63e5a4ac4b0d1e5c3efbb034781fb6fc9ec0800c
Izaias Neto <izaiasneto4@gmail.com>
2020-03-31 12:43:04 -0300

    Finshed the contact page main content

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import './styles.css';
-import menu from './menu';
-import contact from './contact';
-import footer from './footer';
+import menu from './modules/menu';
+import contact from './modules/contact';
+import footer from './modules/footer';
 
 const menuBtn = document.querySelector('.menu-btn');
 const contactBtn = document.querySelector('.contact-btn');
-const mainSection = document.createElement('div').classList.add('main');
+const mainSection = document.querySelector('.main');
 
 const toggleContent = (content) => {
   mainSection.innerHTML = '';
   mainSection.appendChild(content);
-  mainSection.appendChild(footer());
-  document.body.appendChild(mainSection);
+  // mainSection.appendChild(footer());
+  // document.body.appendChild(mainSection);
 };
 
-menuBtn.addEventListener('click', () => toggleContent(menu));
+const firstPage = () => {
+  const h2 = document.createElement('h2');
+  h2.innerHTML = 'The true flavors of Japan will surprise you.';
+  mainSection.appendChild(h2);
+};
+
+firstPage();
+
+menuBtn.addEventListener('click', () => toggleContent(menu()));
 
 contactBtn.addEventListener('click', () => {
   toggleContent(contact());
--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,31 +1,47 @@
 
 const contact = () => {
-  const mainForm = document.createElement('main').classList.add('main-form');
-  // const form = document.createElement('form');
-  // form.innerHTML = 'hey this is a form';
-  // mainForm.innerHTML(form);
-  return mainForm;
-  // form.innerHTML = 'this is an example form';
-  // const nameLabel = document.createElement('label').setAttribute('for', 'name');
-  // const nameInput = document.createElement('input').setAttribute('type', 'text').setAttribute('name', 'name');
-  // const lNameLabel = document.createElement('label').setAttribute('for', 'last-name');
-  // const lNameInput = document.createElement('input').setAttribute('type', 'text').setAttribute('name', 'last-name');
-  // const emailLabel = document.createElement('label').setAttribute('for', 'email');
-  // const emailInput = document.createElement('input').setAttribute('name', 'email');
-  // const messageLabel = document.createElement('label').setAttribute('for', 'message');
-  // const message = document.createElement('textarea').setAttribute('cols', 30).setAttribute('rows', 10).setAttribute('name', 'message');
-  // const submit = document.createElement('input').setAttribute('type', 'submit');
+  const mainForm = document.createElement('main');
+  mainForm.classList.add('main-form');
+  const form = document.createElement('form');
+  mainForm.appendChild(form);
 
-  // form.appendChild(nameLabel);
-  // form.appendChild(nameInput);
-  // form.appendChild(lNameLabel);
-  // form.appendChild(lNameInput);
-  // form.appendChild(emailLabel);
-  // form.appendChild(emailInput);
-  // form.appendChild(messageLabel);
-  // form.appendChild(message);
-  // form.appendChild(submit);
-  // return mainForm;
+  const nameLabel = document.createElement('label');
+  nameLabel.innerHTML = 'Name:';
+  form.appendChild(nameLabel);
+
+  const nameInput = document.createElement('input');
+  nameInput.placeholder = 'Ex: John';
+  form.appendChild(nameInput);
+
+  const lNameLabel = document.createElement('label');
+  lNameLabel.innerHTML = 'Last Name:';
+  form.appendChild(lNameLabel);
+
+  const lNameInput = document.createElement('input');
+  lNameInput.placeholder = 'Ex: Smith';
+  form.appendChild(lNameInput);
+
+  const emailLabel = document.createElement('label');
+  emailLabel.innerHTML = 'Email:';
+  form.appendChild(emailLabel);
+
+  const emailInput = document.createElement('input');
+  emailInput.placeholder = 'Ex: john@smith.com';
+  form.appendChild(emailInput);
+
+  const messageLabel = document.createElement('label');
+  messageLabel.innerHTML = 'Your Message:';
+  form.appendChild(messageLabel);
+
+  const message = document.createElement('textarea');
+  form.appendChild(message);
+
+
+  const submit = document.createElement('button');
+  submit.innerHTML = 'Send Message';
+  form.appendChild(submit);
+
+  return mainForm;
 };
 
 
