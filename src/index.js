da7577995f773aa6fd6f1a4684b1665e7ef1b0c7
Izaias Neto <izaiasneto4@gmail.com>
2020-03-31 16:14:37 -0300

    Finished webpack with menu page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './styles.css';
 import menu from './modules/menu';
 import contact from './modules/contact';
 import footer from './modules/footer';
-import navbar from './modules/navbar';
 
 const menuBtn = document.querySelector('.menu-btn');
 const contactBtn = document.querySelector('.contact-btn');
@@ -14,7 +13,6 @@ const toggleContent = (content) => {
 };
 
 const firstPage = () => {
-  document.body.appendChild(navbar());
   const h2 = document.createElement('h2');
   h2.innerHTML = 'The true flavors of Japan will surprise you.';
   mainSection.appendChild(h2);
